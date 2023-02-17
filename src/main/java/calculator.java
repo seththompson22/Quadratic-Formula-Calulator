15754013e6614715de7163916d158a94d00f8b23
Seth Thompson <thompsonrussell22@gmail.com>
2023-02-17 17:21:33 -0500

    Created .gitignore

---
+++ src/main/java/calculator.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class calculator {
+    public static void main(String[] args) {
+        System.out.println("Welcome to the Quadratic Formula Calculator!");
+        System.out.println("Enter values for a, b, and c as decimal numbers:");
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("a: ");
+        double a = sc.nextDouble();
+        System.out.println("b: ");
+        double b = sc.nextDouble();
+        System.out.println("c: ");
+        double c = sc.nextDouble();
+
+
+    }
+}
--- src/main/java/quadraticCalculator.java
+++
@@ -1,5 +0,0 @@
-public class quadraticCalculator {
-    public static void main(String[] args) {
-        System.out.println("Welcome to the Quadratic Formula Calculator!");
-    }
-}
